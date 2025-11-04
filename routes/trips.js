7fed8985b40f085133515de236423663e3dc6d2f
Aubry Gamard <aubry.soriano@gmail.com>
2025-11-04 18:27:43 +0400

    route get av gestion casse et date

--- routes/trips.js
+++ routes/trips.js
@@ -1,15 +1,25 @@
 var express = require("express");
 var router = express.Router();
 require("../models/connection");
+const moment = require("moment");
+
 const Trip = require("../models/trips");
 
 router.get("/", async (req, res) => {
-  const { departure, arrival, date } = req.body;
+  const { departure, arrival, date } = req.query;
+  console.log(departure, arrival, date);
+
+  const startOfDay = moment(date).startOf("day").toDate();
+  const endOfDay = moment(date).endOf("day").toDate();
+  console.log(startOfDay, endOfDay);
   const trips = await Trip.find({
-    $and: [{ departure }, { arrival }, { date }],
+    departure: { $regex: new RegExp(`^${departure}$`, "i") },
+    arrival: { $regex: new RegExp(`^${arrival}$`, "i") },
+    date: { $gte: startOfDay, $lte: endOfDay },
   });
+  console.log(trips[0].date);
   if (!trips[0]) res.json({ result: false });
-  else res.json({ trips });
+  else res.json({ result: true, trips });
 });
 
 module.exports = router;
